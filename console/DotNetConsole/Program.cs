5a8b9c549bb1438b40456f22cbaf2bcea4c610f5
Purushottam Gaurav <32882662+purushottamgaurav@users.noreply.github.com>
2026-06-21 21:44:40 +0530

    Focus/21 06 (#8)
    * theory ques implemented
    
    * method modified

--- console/DotNetConsole/Program.cs
+++ console/DotNetConsole/Program.cs
@@ -1,20 +1,44 @@
 ﻿using DotNetConsole;
 
-// Reflection example
-//Basics.Reflection(typeof(Basics));
+Console.WriteLine("Select one from the below options: - (1/2/3/4)");
+Console.WriteLine("1 - Coding");
+Console.WriteLine("2 - Oops");
+Console.WriteLine("3 - Design Pattern");
+Console.WriteLine("4 - Solid Prinicpal");
 
-//Basics.EqualsVsDoubleEquals();
+int number = IsValid();
 
-// OOps example
-//Oops.ConstructorTest();
+switch (number)
+{
+    case 1:
+        Console.WriteLine("Please enter the coding number to execute");
+        int quesNo = int.Parse(Console.ReadLine());
+        Type type = typeof(Coding);
+        var methods = type.GetMethods();
 
-// Coding example
+        var method = methods.First(x => x.Name.Contains($"Q{quesNo}_"));
 
-Console.WriteLine("Please enter the coding number to execute");
-int quesNo = int.Parse(Console.ReadLine());
-Type type = typeof(Coding);
-var methods = type.GetMethods();
+        method.Invoke(null, null);
+        break;
 
-var method = methods.First(x => x.Name.Contains($"Q{quesNo}_"));
+    case 2:
+        //StaticTest.StaticTestt();
+        AbstractTest.Test();
+        break;
 
-method.Invoke(null, null);
+    case 3:
+        break;
+}
+
+int IsValid()
+{
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            return number;
+        }
+
+        Console.WriteLine("Please enter a value like 1, 2, or 3");
+    }
+}
--- web/DotNetWebApi/Models/Employee.cs
+++ web/DotNetWebApi/Models/Employee.cs
@@ -1,8 +1,12 @@
-﻿namespace DotNetWebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetWebApi.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
         public int Age { get; set; }
         public string MobileNo { get; set; }
