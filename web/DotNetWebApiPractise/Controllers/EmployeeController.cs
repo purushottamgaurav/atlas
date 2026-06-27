3cfe69f62ba88bbfe5257e522f257c7067b10755
Purushottam Gaurav <purushottamgaurav@outlook.com>
2026-06-27 14:17:54 +0530

    Revert "Focus/26 06 (#14)"
    This reverts commit f377f6a2d03b38491b008f4d451c3555f8e77206.

---
+++ web/DotNetWebApiPractise/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using DotNetWebApiPractise.Custom;
+using DotNetWebApiPractise.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetWebApiPractise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [ActionFilter]
+        [LoggingMethodName]
+        [HttpGet()]
+        public async Task<IActionResult> GetAllEmployee()
+        {
+            var a = _employeeService.GetAllEmployees();
+            return Ok(a);
+        }
+    }
+}
